d01abee2956703d4066a93a9313baaef8b7e09b5
0xk404 <0xk404@gmail.com>
2021-05-09 12:45:06 +0100

    ..

--- adduniquestring.sh
+++ adduniquestring.sh
@@ -1,8 +1,12 @@
 #/bin/bash
 # smacked together by k404
+red="\e[0;91m"
+green="\e[0;92m"
+bold="\e[1m"
+reset="\e[0m"
 if [[ "$1" = "" || "$2" = "" ]]
 then
-  echo "Add what string to what file?"
+  echo -e "${red}${bold}Add what string to what file?${reset}"
 else
   # check if file exists
   if test -f "$2"
@@ -10,14 +14,15 @@ else
       # check if string in file
       if [ ! -z $(grep "$1" "$2") ]
         then
-          echo "Already in file!"
+          echo -e "${red}${bold}Already in file!${reset}"
         else
           echo "$1" >> "$2"
-          echo "Added new string!"
+          echo -e "${green}${bold}Added new string!${reset}"
       fi
     else
+      pwd=$(pwd)
       touch "$2"
       echo "$1" > "$2"
-      echo "Created file and added new string!"
+      echo -e "${green}${bold}Created file ($pwd/$2) and added new string!${reset}"
   fi
 fi
