9c684f3711c12f50d6c1098bec6e574e2b4687a1
0xk404 <0xk404@gmail.com>
2021-05-09 12:11:56 +0100

    ..

--- ipinfoio.sh
+++ ipinfoio.sh
@@ -5,7 +5,6 @@ bold="\e[1m"
 reset="\e[0m"
 if [ "$1" = "" ]
 then
-	#If no ip given getting external ip via ipify.org
 	iploc=$(curl -s ipinfo.io/?token="$ipinfotoken")
 	ip=$(echo $iploc | jq -r .ip)
 	echo -e "${red}${bold} NoIip!\n Getting external ip ... [$ip] \n Ipinfo.io results:${reset}"
@@ -15,7 +14,6 @@ else
 	echo -e "${red}${bold} Ip [$theip] \n Ipinfo.io results:${reset}"
 	iploc=$(curl -s ipinfo.io/"$theip"?token="$ipinfotoken")
 	printf "$iploc\n" | jq .
-	# http://maps.google.com/maps?q=24.197611,120.780512
 fi
 echo -e "${red}${bold} Map:${reset}"
 
